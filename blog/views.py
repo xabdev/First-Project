ea4d84e8c7ffd4ec2022ba9ea43dece65d886a6c
Jav <xab.dev@gmail.com>
2021-10-20 19:46:47 -0300

    post_edit redirect to post_list

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -22,7 +22,7 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('post_list')
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
